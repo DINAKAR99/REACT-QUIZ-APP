36356e75b40ffc4fd7d3460a4016632a8c3d0def
DIN OG ACCOUNT <103622152+DINAKAR99@users.noreply.github.com>
2024-05-31 14:42:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/contact" element={<SupportPage />} />
+          dedew
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </BrowserRouter>
